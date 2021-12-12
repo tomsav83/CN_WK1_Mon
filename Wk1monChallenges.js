9f2c05ae82905a3c196f4f32856891f0aac59d05
Tomsav83 <tomsav83@gmail.com>
2021-12-12 05:14:20 +0000

    init

---
+++ Wk1monChallenges.js
@@ -0,0 +1,28 @@
+// CHALLENGE 1
+
+// const name = 'Tom';
+// let age = 37;
+// const faveColour = 'red';
+// console.log(
+//   `Hi my name is ${name}, I'm ${age} years old and my favourite colour is ${faveColour}.`
+// );
+
+// CHALLENGE 2
+
+// let breakfast = 'toast';
+// let lunch = 'burrito';
+// let dinner = 'mac & cheese';
+
+// console.log(
+//   `Today I had ${breakfast} for breakfast, ${lunch} for lunch & ${dinner} for tea.`
+// );
+
+// CHALLENGE 3
+
+// let myBday = new Date('12/22/1983');
+// let todayDate = new Date('11/01/2021');
+// let msSinceBday = todayDate - myBday;
+// let msInDay = 1000 * 3600 * 24;
+// let daysSinceBday = msSinceBday / msInDay;
+
+// console.log(`I have been on this planet for ${daysSinceBday} days.`);
